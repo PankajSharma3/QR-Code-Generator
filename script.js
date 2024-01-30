649d7b4a99bb94b26fb93555032e1bad81035df8
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-01-30 22:16:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const qrText = document.querySelector('#qrText');
+const qrImage = document.getElementById('qrImage');
+const generateQR = ()=>{
+    let spaces = 0;
+    for(let i=0;i<qrText.value.length;i++){
+        if(qrText.value[i]=== " ")
+        spaces++;
+    }
+    if(spaces!==0){
+        alert("Please enter the text without spaces");
+    }
+    else if(qrText.value===""){
+        console.log(spaces);
+        alert("Please enter some text to be encoded in the QR code");
+    }
+    else{
+        console.log(spaces);
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+(qrText.value);
+    }
+}
